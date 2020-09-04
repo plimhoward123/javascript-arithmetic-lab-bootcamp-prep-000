33b7f83f766593c09e22b4e5216584850548d323
Plimon Howard V <plimon.howard@gmail.com>
2020-09-04 22:53:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,9 +13,13 @@ function multiply(a,b){
 function divide(a,b){
   return a/b
 }
+
 function increment(n){
-  return (n++)
+  n = n++
+  return n
 }
+
 function decrement(n){
-  return (n--)
+  n = n--
+  return n
 }
